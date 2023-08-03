3f7d6bd530b15ba69f299aabbc5f8f69808ca4e9
Pavlo Rytikov <pavel.ritikov@gmail.com>
2023-08-03 17:45:23 +0300

    Init repo

---
+++ src/LearnGoogleCloud.PubSub.Consumer/Program.cs
@@ -0,0 +1,71 @@
+﻿using Google.Api.Gax;
+using Google.Api.Gax.Grpc;
+using Google.Api.Gax.ResourceNames;
+using Google.Cloud.PubSub.V1;
+using Grpc.Core;
+
+var projectId = "wurdum-project";
+var projectName = ProjectName.FormatProject(projectId);
+
+var topicId = "test-1";
+var topicName = TopicName.FromProjectTopic(projectId, topicId);
+
+var subscriptionId = "test-1-sub";
+var subscriptionName = SubscriptionName.FromProjectSubscription(projectId, subscriptionId);
+
+var client = SubscriberServiceApiClient.Create();
+var cts = new CancellationTokenSource();
+var pull = client.StreamingPull(CallSettings.FromCancellationToken(cts.Token), new(10));
+
+await pull.TryWriteAsync(new()
+{
+    SubscriptionAsSubscriptionName = subscriptionName,
+    StreamAckDeadlineSeconds = 20
+});
+
+_ = Task.Run(async () =>
+{
+    await pull.TryWriteAsync(new()
+    {
+        SubscriptionAsSubscriptionName = subscriptionName,
+        ClientId = "1"
+    });
+
+    await foreach (var response in pull.GetResponseStream())
+    {
+        var acks = new List<string>();
+        foreach (var message in response.ReceivedMessages)
+        {
+            Console.WriteLine($"Consumed message {message.Message.MessageId} with data {message.Message.Data.ToStringUtf8()}");
+            client.Acknowledge(subscriptionName, new[] { message.AckId });
+            acks.Add(message.AckId);
+        }
+
+        if (acks.Count > 0)
+        {
+            await pull.TryWriteAsync(new()
+            {
+                AckIds = { acks }
+            });
+        }
+    }
+});
+
+Console.ReadLine();
+cts.Cancel();
+
+
+
+/*try
+{
+    var response = client.Pull(subscriptionName, 10, settings);
+    foreach (var message in response.ReceivedMessages)
+    {
+        Console.WriteLine($"Consumed message {message.Message.MessageId} with data {message.Message.Data.ToStringUtf8()}");
+        client.Acknowledge(subscriptionName, new[] { message.AckId });
+    }
+}
+catch (RpcException exception) when (exception.StatusCode == StatusCode.DeadlineExceeded)
+{
+    Console.WriteLine("No messages to consume!");
+}*/
---
+++ src/LearnGoogleCloud.PubSub.Producer/Program.cs
@@ -0,0 +1,46 @@
+﻿using Google.Api.Gax.ResourceNames;
+using Google.Cloud.PubSub.V1;
+using Google.Protobuf;
+
+var projectId = "wurdum-project";
+var projectName = ProjectName.FormatProject(projectId);
+
+var topicId = "test-1";
+var topicName = TopicName.FromProjectTopic(projectId, topicId);
+
+var subscriptionId = topicId + "-sub";
+var subscriptionName = SubscriptionName.FromProjectSubscription(projectId, subscriptionId);
+
+var publisherServiceClient = PublisherServiceApiClient.Create();
+var topics = publisherServiceClient.ListTopics(projectName);
+var topic = topics.FirstOrDefault(t => t.TopicName == topicName);
+if (topic == null)
+{
+    topic = publisherServiceClient.CreateTopic(topicName);
+    Console.WriteLine($"Topic {topic.TopicName} created");
+}
+
+var subscriberServiceClient = SubscriberServiceApiClient.Create();
+var subscriptions = subscriberServiceClient.ListSubscriptions(projectName);
+var subscription = subscriptions.FirstOrDefault(s => s.TopicAsTopicName == topicName && s.SubscriptionName == subscriptionName);
+if (subscription == null)
+{
+    subscription = subscriberServiceClient.CreateSubscription(subscriptionName, topicName, new(), 30);
+    Console.WriteLine($"Subscription {subscription.SubscriptionName} created");
+}
+
+var messageId = "test1";
+var client = PublisherClient.Create(topicName);
+var message = new PubsubMessage
+{
+    MessageId = messageId,
+    Data = ByteString.CopyFromUtf8("Hello World!!"),
+    Attributes =
+    {
+        { "key1", "value1" },
+        { "key2", "value2" }
+    }
+};
+
+var id = await client.PublishAsync(message);
+Console.WriteLine($"Message {messageId} published with id {id}");
